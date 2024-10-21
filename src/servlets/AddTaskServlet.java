3e8255e39e838d341681a4c7f9c38b96f1abab24
michasoil-3 <182325294+michasoil-3@users.noreply.github.com>
2024-10-21 10:59:15 +0500

    Improved task creation

--- src/servlets/AddTaskServlet.java
+++ src/servlets/AddTaskServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import db.DBManager;
+import functools.Functools;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,43 +10,20 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 @WebServlet(value="/add-task")
 public class AddTaskServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String description = req.getParameter("description");
-        String deadlineStr = req.getParameter("deadline");
-        LocalDateTime deadline = null;
-        if (!deadlineStr.isEmpty()) {
-            deadline = LocalDateTime.parse(deadlineStr);
-        }
-        String category = req.getParameter("category");
-        if (category.equals("N/A")) {
-            category = null;
-        } else {
-            category = category.toLowerCase();
-        }
-        // YES DEADL, YES CAT
-        // YES DEADL, NO CAT
-        // NO DEADL, YES CAT
-        // NO DEADL, NO CAT
-
-        if (!deadlineStr.isEmpty() && category != null) {
-            DBManager.addTask(name, description, deadline, category);
-        }
-        if (!deadlineStr.isEmpty() && category == null) {
-            DBManager.addTask(name, description, deadline);
-        }
-
-        if (deadlineStr.isEmpty() && category != null) {
-            DBManager.addTask(name, description, category);
-        }
-
-        if (deadlineStr.isEmpty() && category == null) {
-            DBManager.addTask(name, description);
-        }
+        HashMap<String, Object> hm = new HashMap<>();
+        Functools.handleParametersHashmap(hm, req);
+        DBManager.addTask(
+                (String) hm.get("name"),
+                (String) hm.get("description"),
+                (LocalDateTime) hm.get("deadline"),
+                (String) hm.get("category")
+        );
         req.getRequestDispatcher("/").forward(req, resp);
     }
 }
