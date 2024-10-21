9e2e3a358a689af62614d1391f9c98bfdade486e
michasoil-3 <182325294+michasoil-3@users.noreply.github.com>
2024-10-21 10:58:13 +0500

    Created edit and delete servlets

---
+++ src/servlets/DeleteTaskServlet.java
@@ -0,0 +1,20 @@
+package servlets;
+
+import db.DBManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value="/delete-task")
+public class DeleteTaskServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("id"));
+        DBManager.removeTask(id);
+        req.getRequestDispatcher("/").forward(req, resp);
+    }
+}
---
+++ src/servlets/EditTaskServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import db.DBManager;
+import functools.Functools;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+@WebServlet(value="/edit-task")
+public class EditTaskServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("id"));
+        HashMap<String, Object> hm = new HashMap<>();
+        Functools.handleParametersHashmap(hm, req);
+        boolean completed = req.getParameter("completed").equals("Yes");
+        DBManager.editTask(
+                id,
+                (String) hm.get("name"),
+                (String) hm.get("description"),
+                (LocalDateTime) hm.get("deadline"),
+                (String) hm.get("category"),
+                completed
+        );
+        req.getRequestDispatcher("/").forward(req, resp);
+    }
+}
